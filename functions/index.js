d57a8f0133b546421ef95df8aa53625aaa038b1e
Ariel Hernandez <arielhr1987@gmail.com>
2020-05-14 19:48:36 -0700

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -20,16 +20,18 @@ exports.generate = functions.https.onRequest((request, response) => {
     /**
      * Headers
      */
-    response.set({
-        'Pragma': 'public',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': 'true',
-        'Access-Control-Allow-Methods': 'GET, OPTIONS',
-        'Access-Control-Max-Age': '1814400',
-        'Access-Control-Allow-Headers': 'Content-Type, Accept, X-Requested-With, remember-me',
-        'Cache-Control': 'max-age=1814400',
-        'Content-Type': 'image/svg+xml',
-    })
+    let cacheTime = 60 * 60 * 24 * 30; //1 month
+    response.set('Content-Type', 'image/svg+xml');
+    response.set('Access-Control-Allow-Origin', '*');
+    response.set('Cache-Control', 'public, max-age=' + cacheTime + ', s-maxage=' + cacheTime)
+    // response.set({
+    //     'Pragma': 'public',
+    //     'Access-Control-Allow-Origin': '*',
+    //     'Access-Control-Allow-Credentials': 'true',
+    //     'Access-Control-Allow-Methods': 'GET, OPTIONS',
+    //     'Access-Control-Max-Age': '1814400',
+    //     'Access-Control-Allow-Headers': 'Content-Type, Accept, X-Requested-With, remember-me',
+    // });
 
     /**
      * Validations
@@ -57,15 +59,17 @@ exports.generate = functions.https.onRequest((request, response) => {
     }
 
     let hexRegex = /^#?([0-9A-F]{3}){1,2}$/i;
+    let rgbaRegex = /^rgba?\((\d{1,3}%?),\s*(\d{1,3}%?),\s*(\d{1,3}%?),\s*(\d*(?:\.\d+)?)\)$/i;
     let background = hexRegex.test(request.query.background) ? request.query.background.replace('#', '') : 'fc91ad';
 
     let color = (parseInt(background, 16) > 0xffffff / 2) ? '000' : 'fff';
-    color = hexRegex.test(request.query.color) ? request.query.color.replace('#', '') : color;
+    color = hexRegex.test(request.query.color) ? ('#' + request.query.color.replace('#', '')) : color;
+    color = rgbaRegex.test(request.query.color) ? request.query.color : color;
 
     /**
      * Create avatar
      */
-    let avatar = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="' + size + 'px" height="' + size + 'px" viewBox="0 0 ' + size + ' ' + size + '" style="user-select: none;" version="1.1"><' + (rounded ? 'circle' : 'rect') + ' fill="#' + background + '" cx="' + (size / 2) + '" width="' + size + '" height="' + size + '" cy="' + (size / 2) + '" r="' + (size / 2) + '"/><text x="50%" y="50%" style="color: #' + color + '; line-height: 1;font-family: -apple-system, BlinkMacSystemFont, \'Segoe UI\', \'Roboto\', \'Oxygen\', \'Ubuntu\', \'Fira Sans\', \'Droid Sans\', \'Helvetica Neue\', sans-serif;" alignment-baseline="middle" text-anchor="middle" font-size="' + font_size + '" font-weight="' + (bold ? 600 : 400) + '" dy=".1em" dominant-baseline="middle" fill="#' + color + '">' + text + '</text></svg>';
+    let avatar = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="' + size + 'px" height="' + size + 'px" viewBox="0 0 ' + size + ' ' + size + '" style="user-select: none;" version="1.1"><' + (rounded ? 'circle' : 'rect') + ' fill="#' + background + '" cx="' + (size / 2) + '" width="' + size + '" height="' + size + '" cy="' + (size / 2) + '" r="' + (size / 2) + '"/><text x="50%" y="50%" style="color: ' + color + '; line-height: 1;font-family: -apple-system, BlinkMacSystemFont, \'Segoe UI\', \'Roboto\', \'Oxygen\', \'Ubuntu\', \'Fira Sans\', \'Droid Sans\', \'Helvetica Neue\', sans-serif;" alignment-baseline="middle" text-anchor="middle" font-size="' + font_size + '" font-weight="' + (bold ? 600 : 400) + '" dy=".1em" dominant-baseline="middle" fill="' + color + '">' + text + '</text></svg>';
 
     /**
      * Send response
